44ff50474f4f78c4c4b9de4b738afa5b1393c1d4
aanandshekharroy <aanandshekharroy@gmail.com>
2016-12-02 01:14:26 +0530

    feat:Add SQLDBHelper

---
+++ Teleprompter/app/src/main/java/com/example/theseus/teleprompter/data/ScriptDBHelper.java
@@ -0,0 +1,33 @@
+package com.example.theseus.teleprompter.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by theseus on 2/12/16.
+ */
+
+public class ScriptDBHelper extends SQLiteOpenHelper {
+    public static final String DATABASE_NAME="scripts.db";
+    public static final int DATABASE_VERSION=1;
+    public ScriptDBHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        final String SQL_QUERY="CREATE TABLE "+ ScriptContract.ScriptEntry.TABLE_NAME
+            +" ( "+ ScriptContract.ScriptEntry._ID + " INTEGER PRIMARY KEY, " +
+                ScriptContract.ScriptEntry.COLUMN_TITLE+" TEXT NOT NULL, "+
+                ScriptContract.ScriptEntry.COLUMN_CONTENT+" TEXT NOT NULL );";
+
+        db.execSQL(SQL_QUERY);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS "+ ScriptContract.ScriptEntry.TABLE_NAME);
+        onCreate(db);
+    }
+}
