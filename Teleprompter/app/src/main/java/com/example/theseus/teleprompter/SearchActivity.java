88e94f67bb254ede28db1c6d6e8a3c619441bb1e
aanandshekharroy <aanandshekharroy@gmail.com>
2016-12-14 05:33:30 +0530

    feat:Expanded toolbar of search and Recyclerview implemented in search adapter

--- Teleprompter/app/src/main/java/com/example/theseus/teleprompter/SearchActivity.java
+++ Teleprompter/app/src/main/java/com/example/theseus/teleprompter/SearchActivity.java
@@ -9,12 +9,17 @@ import android.content.Loader;
 import android.content.CursorLoader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
+import android.util.AttributeSet;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
 import com.example.theseus.teleprompter.adapter.SearchAdapter;
@@ -28,6 +33,7 @@ public class SearchActivity extends AppCompatActivity implements SearchView.OnQu
     private Context mContext;
     private String mQuery="";
     private SearchAdapter mSearchAdapter;
+    private RecyclerView mRecyclerView;
     public static String[] SCRIPT_POJECTION=new String[]{
             ScriptContract.ScriptEntry._ID,
             ScriptContract.ScriptEntry.COLUMN_TITLE,
@@ -42,8 +48,12 @@ public class SearchActivity extends AppCompatActivity implements SearchView.OnQu
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         mContext=this;
         mSearchAdapter=new SearchAdapter(mContext);
+        mRecyclerView=(RecyclerView)findViewById(R.id.recycler_view);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerView.setAdapter(mSearchAdapter);
         getLoaderManager().initLoader(LOADER_ID,null, this);
     }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
@@ -54,9 +64,10 @@ public class SearchActivity extends AppCompatActivity implements SearchView.OnQu
                 (SearchView) menu.findItem(R.id.search).getActionView();
         searchView.setSearchableInfo(
                 searchManager.getSearchableInfo(getComponentName()));
-        searchView.setIconifiedByDefault(true);
+        searchView.setIconifiedByDefault(false);
         searchView.setSubmitButtonEnabled(true);
         searchView.setOnQueryTextListener(this);
+        searchView.requestFocus();
         return true;
     }
 
@@ -65,46 +76,27 @@ public class SearchActivity extends AppCompatActivity implements SearchView.OnQu
         int id = item.getItemId();
         switch (id){
             case R.id.search_menu:
-                Toast.makeText(mContext,"clicked search",Toast.LENGTH_SHORT).show();
+//                Toast.makeText(mContext,"clicked search",Toast.LENGTH_SHORT).show();
                 return true;
         }
         return super.onOptionsItemSelected(item);
 
     }
     @Override
-    protected void onNewIntent(Intent intent) {
-//        ...
-        handleIntent(intent);
-    }
-
-    private void handleIntent(Intent intent) {
-
-        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
-//            String query = intent.getStringExtra(SearchManager.QUERY);
-            //use the query to search your data somehow
-        }
-    }
-
-
-    @Override
     public boolean onQueryTextSubmit(String query) {
-//        Toast.makeText(mContext,query,Toast.LENGTH_SHORT).show();
         mQuery=query;
         return false;
     }
 
     @Override
     public boolean onQueryTextChange(String newText) {
-//        Log.d(LOG)
         mQuery=newText;
         getLoaderManager().restartLoader(LOADER_ID,null,this);
-        Toast.makeText(mContext,newText,Toast.LENGTH_SHORT).show();
         return false;
     }
 
     @Override
     public Loader<Cursor> onCreateLoader(int id, Bundle args) {
-        Log.d(LOG_TAG,"uri for search: "+ ScriptContract.ScriptEntry.buildUriFromKey(mQuery));
         return new CursorLoader(mContext, ScriptContract.ScriptEntry.buildUriFromKey(mQuery),SCRIPT_POJECTION,null,null,null);
     }
 
@@ -113,13 +105,13 @@ public class SearchActivity extends AppCompatActivity implements SearchView.OnQu
         Log.d(LOG_TAG,"data count: "+data);
         if(data!=null){
             Log.d(LOG_TAG,"data count: "+data.getCount());
-//            mSearchAdapter.swapCursor(data);
+            mSearchAdapter.swapCursor(data);
         }
 
     }
 
     @Override
     public void onLoaderReset(Loader<Cursor> loader) {
-//        mSearchAdapter.swapCursor(null);
+        mSearchAdapter.swapCursor(null);
     }
 }
--- Teleprompter/app/src/main/java/com/example/theseus/teleprompter/adapter/SearchAdapter.java
+++ Teleprompter/app/src/main/java/com/example/theseus/teleprompter/adapter/SearchAdapter.java
@@ -3,19 +3,59 @@ package com.example.theseus.teleprompter.adapter;
 import android.content.Context;
 import android.database.Cursor;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.theseus.teleprompter.MainActivityFragment;
+import com.example.theseus.teleprompter.R;
 
 /**
  * Created by theseus on 13/12/16.
  */
 
-public class SearchAdapter {
-    private SearchAdapter mSearchAdapter;
-    private RecyclerView mRecyclerView;
+public class SearchAdapter extends RecyclerView.Adapter<SearchAdapter.SearchAdapterViewHolder> {
     private Context mContext;
+    private Cursor mCursor;
     public SearchAdapter(Context context) {
         mContext=context;
     }
 
     public void swapCursor(Cursor data) {
+        mCursor=data;
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public SearchAdapterViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.script_search_item,parent,false);
+
+        return new SearchAdapterViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(SearchAdapterViewHolder holder, int position) {
+        mCursor.moveToPosition(position);
+        holder.mTitleTextView.setText(mCursor.getString(MainActivityFragment.COLUMN_TITLE));
+        holder.mContentView.setText(mCursor.getString(MainActivityFragment.COLUMN_CONTENT));
+    }
+
+    @Override
+    public int getItemCount() {
+        if(mCursor==null){
+            return 0;
+        }
+        return mCursor.getCount();
+    }
+
+    public class SearchAdapterViewHolder extends RecyclerView.ViewHolder{
+        final TextView mTitleTextView;
+        final TextView mContentView;
+        public SearchAdapterViewHolder(View itemView) {
+            super(itemView);
+            mTitleTextView =(TextView) itemView.findViewById(R.id.title);
+            mContentView=(TextView)itemView.findViewById(R.id.content);
+        }
     }
 }
